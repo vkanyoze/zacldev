2f980bac01efb8192909a63259e1651a265e631f
mr_kanyoze <vkanyoze@gmail.com>
2025-08-01 14:01:39 +0200

    Updated Card class name

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 use OwenIt\Auditing\Contracts\Auditable;
 use OwenIt\Auditing\Auditable as AuditableTrait;
   
-class card extends Model implements Auditable
+class Card extends Model implements Auditable
 {
     use HasFactory, Uuids, AuditableTrait;
   
